0903e0466a5d6e9cc9e3cf051fa935b78856cab0
erasonj <75148758+erasonj@users.noreply.github.com>
2021-08-08 19:44:50 +0800

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,9 @@
+package daily70;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println(" ");
+        Solution s = new Solution();
+        System.out.println(s.climbStairs(5));
+    }
+}
